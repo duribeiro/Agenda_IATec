f263b9b8c42308bf9975575398740abc1c17e382
duribeiro <eduardo.duduribeiro1@gmail.com>
2022-04-05 18:01:37 -0400

    Cria os arquivos da camada model, User, Event e UserEvent

---
+++ Agenda IATec/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agenda_IATec.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public string Local { get; set; }
+        public int Tipo { get; set; }
+        public List<UserEvent> UsersEvents { get; set; }
+    }
+}

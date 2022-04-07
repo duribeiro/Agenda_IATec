ca6871046d3b1f67316d9b87738700bd29a40d82
duribeiro <eduardo.duduribeiro1@gmail.com>
2022-04-07 09:45:56 -0400

    Cria relacao 1:N entre Event e User do identity

--- Agenda IATec/Models/Event.cs
+++ Agenda IATec/Models/Event.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +16,11 @@ namespace Agenda_IATec.Models
         public DateTime Data { get; set; }
         public string Local { get; set; }
         public int Tipo { get; set; }
-        public List<UserEvent> UsersEvents { get; set; }
+
+        [Display(Name = "Usuário")]
+        [ForeignKey("IdentityUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+        public virtual IdentityUser IdentityUser { get; set; }
     }
 }
